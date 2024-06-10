76b811c96f0a74339fb23a6ae5b8b6bea8c52f3c
Jonas Alexander Støbakk <jonas.stobakk@bouvet.no>
2024-06-10 15:50:55 +0200

    Init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import { AuthUser } from "./components/firebase/AuthUser";
-import { CreateUser } from "./components/firebase/CreateUser";
+// import { AuthUser } from "./components/firebase/AuthUser";
+// import { CreateUser } from "./components/firebase/CreateUser";
+import PatientDetails from "./components/PatientDetails/PatientDetails";
+/* <AuthUser />
+<br />
+<CreateUser />   */
 
 function App() {
   return (
     <>
-      <AuthUser />
-      <br />
-      <CreateUser />
+      <PatientDetails/>
     </>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/PatientDetails/PatientDetails.tsx
@@ -0,0 +1,18 @@
+import styles from "./PatientDetails.module.css";
+
+export default function PatientDetails() {
+	return (
+		<>
+			<div className={styles.patientDetailsWrapper}>
+				<div className={styles.patientInfoBlock}>
+					<img src="https://placehold.co/85x85" alt="Profile picture" />
+					<div className={styles.PatientInfo}>
+						<h1>Patient Name</h1>
+						<p>Age</p>
+					</div>
+				</div>
+				<p className={styles.signedInAs}>Signed in as: First name</p>
+			</div>
+		</>
+	);
+}
