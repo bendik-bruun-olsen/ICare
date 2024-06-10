50fe29c1d9e09ff50b109605665cc7e6faa5f443
Kokila Chandramohan <kokila.chandramohan@bouvet.no>
2024-06-10 11:02:22 +0200

    Update routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,75 @@
-import { AuthUser } from "./components/firebase/AuthUser";
-import { CreateUser } from "./components/firebase/CreateUser";
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/HomePage";
+import Login from "./pages/LoginPage";
 
-function App() {
-  return (
-    <>
-      <AuthUser />
-      <br />
-      <CreateUser />
-    </>
-  );
-}
+import Logout from "./pages/LogoutPage";
+import Signup from "./pages/SignupPage";
+import Todo from "./pages/TodoPage";
+import AddTodo from "./pages/AddTodo";
+import EditTodo from "./pages/EditTodoPage";
+import Appointment from "./pages/AppointmentPage";
+import AddAppointment from "./pages/AddAppointment";
+import EditAppointment from "./pages/EditAppointmentPage";
+import Contact from "./pages/ContactPage1";
+import NotFound from "./pages/NotFound";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/:Login",
+    element: <Login />,
+  },
+  {
+    path: "/:Logout",
+    element: <Logout />,
+  },
+
+  {
+    path: "/:Signup",
+    element: <Signup />,
+  },
+  {
+    path: "/:Todo",
+    element: <Todo />,
+  },
+  {
+    path: "/:AddTodo",
+    element: <AddTodo />,
+  },
+  {
+    path: "/:EditTodo",
+    element: <EditTodo />,
+  },
+  {
+    path: "/:Appointment",
+    element: <Appointment />,
+  },
+  {
+    path: "/:AddAppointment",
+    element: <AddAppointment />,
+  },
+  {
+    path: "/:EditAppointment",
+    element: <EditAppointment />,
+  },
+
+  {
+    path: "/:contact",
+    element: <Contact />,
+  },
+
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+]);
+
+const App: React.FC = () => {
+  return <RouterProvider router={router} />;
+};
 
 export default App;
