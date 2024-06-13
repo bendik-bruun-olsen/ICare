70de6704485aa6c5da2babd5bcd46a32dc60f8b3
Jonas Alexander Støbakk <jonas.stobakk@bouvet.no>
2024-06-13 13:15:09 +0200

    Fixed App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,12 @@ import React from "react";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes";
 import { StateContextProvider } from "./context/StateContext";
-import ToDoHome from "./components/todohome/ToDoHome";
 
 const App: React.FC = () => {
 	return (
-		// <StateContextProvider>
-		// 	<RouterProvider router={router} />;
-		// </StateContextProvider>
-		<ToDoHome />
+		<StateContextProvider>
+			<RouterProvider router={router} />;
+		</StateContextProvider>
 	);
 };
 
