e31b6037090ad109e0887be7ec79381b668c0d5e
Jonas Alexander Støbakk <jonas.stobakk@bouvet.no>
2024-06-13 11:02:42 +0200

    Did some coding

--- src/components/taskcontainer/TaskContainer.tsx
+++ src/components/taskcontainer/TaskContainer.tsx
@@ -1,19 +1,28 @@
 import styles from "./TaskContainer.module.css";
+import { Icon } from "@equinor/eds-core-react";
+import { comment, more_horizontal, badge } from "@equinor/eds-icons";
 
 export default function TaskContainer() {
 	return (
-		<div>
+		<div className={styles.ToDoWrapper}>
 			<div>
 				<h1>08:00 - Morning Walk</h1>
 			</div>
-			<div>
-				<p className={styles.DescriptionSection}>
+			<div className={styles.DescriptionSection}>
+				<p>
 					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
 					elementum tempor facilisis.
 				</p>
+				<Icon data={more_horizontal} size={48} className={styles.moreIcon} />
 			</div>
-			<div>
-				<p>Comment</p>
+			<div className={styles.CommentWrapper}>
+				<div className={styles.CommentSection}>
+					<div className={styles.iconContainer}>
+						<Icon data={comment} size={16} />
+					</div>
+					<p>Comment</p>
+				</div>
+				<Icon data={badge} size={24} className={styles.badgeIcon} />
 			</div>
 		</div>
 	);
