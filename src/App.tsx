69906e3806dd45d3e47a1136a6fe6d2f5ecc97de
Jonas Alexander Støbakk <jonas.stobakk@bouvet.no>
2024-06-18 10:18:26 +0200

    Fixed App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,12 @@ import React from "react";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes";
 import { StateContextProvider } from "./context/StateContext";
-import AppointmentsQuickView from "./components/AppointmentsQuickView/AppointmentsQuickView";
 
 const App: React.FC = () => {
 	return (
-		// <StateContextProvider>
-		// 	<RouterProvider router={router} />;
-		// </StateContextProvider>
-		<AppointmentsQuickView />
+		<StateContextProvider>
+			<RouterProvider router={router} />;
+		</StateContextProvider>
 	);
 };
 
