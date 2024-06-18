b71ab6fb70001a3494cada9e5ec9acc2d6b8caa1
Jonas Alexander Støbakk <jonas.stobakk@bouvet.no>
2024-06-18 10:15:19 +0200

    Init commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes";
 import { StateContextProvider } from "./context/StateContext";
-import AppointmentComponent from "./components/appointmentcomponent/AppointmentComponent";
+import AppointmentsQuickView from "./components/AppointmentsQuickView/AppointmentsQuickView";
 
 const App: React.FC = () => {
 	return (
 		// <StateContextProvider>
 		// 	<RouterProvider router={router} />;
 		// </StateContextProvider>
-		<AppointmentComponent />
+		<AppointmentsQuickView />
 	);
 };
 
--- src/components/AppointmentsQuickView/AppointmentsQuickView.tsx
+++ src/components/AppointmentsQuickView/AppointmentsQuickView.tsx
@@ -1,16 +1,21 @@
 import { Icon } from "@equinor/eds-core-react";
 import { calendar_today, arrow_forward } from "@equinor/eds-icons";
-import styles from "./AppointmentComponent.module.css";
+import styles from "./AppointmentsQuickView.module.css";
 
-export default function AppointmentComponent() {
+export default function AppointmentsQuickView() {
 	return (
 		<div className={styles.appointmentsWrapper}>
 			<div>
-				<p>08:30 - Meeting with doctor</p>
+				<p className={styles.appointmentsText}>08:30 - Meeting with doctor</p>
 				<p>11:00 - Meeting with chiropractor</p>
 			</div>
 			<div className={styles.appointmentIconWrapper}>
-				<Icon data={calendar_today} size={48} />
+				<Icon
+					data={calendar_today}
+					size={48}
+					color={"#aacccf"}
+					className={styles.calendarIcon}
+				/>
 				<Icon data={arrow_forward} size={24} />
 			</div>
 		</div>
