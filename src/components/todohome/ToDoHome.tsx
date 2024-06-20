42c619b452451e255704b1d80e04a61fcdebda55
Kokila Chandramohan <kokila.chandramohan@bouvet.no>
2024-06-20 10:00:32 +0200

    Initial commit

--- src/components/todohome/ToDoHome.tsx
+++ src/components/todohome/ToDoHome.tsx
@@ -3,27 +3,27 @@ import { restaurant, group, walk, placeholder_icon } from "@equinor/eds-icons";
 import styles from "./ToDoHome.module.css";
 
 export default function ToDoHome() {
-	const icons = {
-		food: restaurant,
-		social: group,
-		exercise: walk,
-		medicine: placeholder_icon,
-	};
+  const icons = {
+    food: restaurant,
+    social: group,
+    exercise: walk,
+    medicine: placeholder_icon,
+  };
 
-	return (
-		<div className={styles.toDoWrapper}>
-			<div>
-				<h1 className={styles.toDoText}>Category</h1>
-				<h1>0/5 tasks</h1>
-			</div>
-			<div className={styles.icon}>
-				<Icon
-					data={icons.food}
-					size={48}
-					color={"#F5F5F5"}
-					className={styles.iconOpacity}
-				></Icon>
-			</div>
-		</div>
-	);
+  return (
+    <div className={styles.toDoWrapper}>
+      <div>
+        <h1 className={styles.toDoText}>Category</h1>
+        <h1>0/5 tasks</h1>
+      </div>
+      <div className={styles.icon}>
+        <Icon
+          data={icons.food}
+          size={48}
+          color={"#F5F5F5"}
+          className={styles.iconOpacity}
+        ></Icon>
+      </div>
+    </div>
+  );
 }
