06a595cec92e5302ad91bc26fdda8420c8ba01da
Jonas Alexander Støbakk <jonas.stobakk@bouvet.no>
2024-06-20 13:05:44 +0200

    Small changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes";
 import { StateContextProvider } from "./context/StateContext";
+import TaskContainer from "./components/TaskContainer/TaskContainer";
 
 const App: React.FC = () => {
 	return (
-		<StateContextProvider>
-			<RouterProvider router={router} />;
-		</StateContextProvider>
+		// <StateContextProvider>
+		// 	<RouterProvider router={router} />;
+		// </StateContextProvider>
+		<TaskContainer />
 	);
 };
 
