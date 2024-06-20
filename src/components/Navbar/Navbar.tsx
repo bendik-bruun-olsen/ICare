89dd3f70723f7c972b818aab01048ddb571383c5
Bendik Haakestad Bruun-Olsen <bendik.bruun-olsen@bouvet.no>
2024-06-20 12:02:13 +0200

    Added useLocation to get the current path/pagetitle

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,14 +2,25 @@ import styles from "./Navbar.module.css";
 import { Icon } from "@equinor/eds-core-react";
 import { account_circle } from "@equinor/eds-icons";
 import Logo from "../Logo/Logo";
+import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
 
 export default function Navbar() {
+	const location = useLocation();
+
+	useEffect(() => {
+		console.log("pathname: ", location.pathname);
+	}, [location]);
+
 	return (
 		<nav className={styles.navbar}>
 			<Logo />
-			<div className={styles.container}>
+			<div className={styles.pageTitleContainer}>
+				<h1> {location.pathname}</h1>
+			</div>
+			<div className={styles.userContainer}>
 				<Icon
-					className={styles.hamburgerMenuIcon}
+					className={styles.userIcon}
 					data={account_circle}
 					size={40}
 					// onClick={}
--- src/pages/Login/LoginPage.tsx
+++ src/pages/Login/LoginPage.tsx
@@ -4,74 +4,78 @@ import { useState, useContext, useEffect } from "react";
 import { Input, Label, Button } from "@equinor/eds-core-react";
 import { Styled } from "styled-components";
 import { StateContext } from "../../context/StateContext";
-import Logo from "../assets/images/Logo.png";
+import logo from "../../assets/logo.png";
 import { useNavigate } from "react-router-dom";
 import { Paths } from "../../utils/paths";
 import "./login.modules.css";
 
 export default function LoginPage() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const context = useContext(StateContext);
-  const { user, setUser } = context;
-  const navigate = useNavigate();
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const context = useContext(StateContext);
+	const { user, setUser } = context;
+	const navigate = useNavigate();
 
-  const signIn = async () => {
-    try {
-      console.log("test");
+	const signIn = async () => {
+		try {
+			console.log("test");
 
-      const userdata = await signInWithEmailAndPassword(auth, email, password);
-      // console.log("user: ", JSON.stringify(user));
+			const userdata = await signInWithEmailAndPassword(
+				auth,
+				email,
+				password
+			);
+			// console.log("user: ", JSON.stringify(user));
 
-      //   return redirect(Paths.HOME);
-      // }
-      console.log(userdata);
+			//   return redirect(Paths.HOME);
+			// }
+			console.log(userdata);
 
-      setUser(userdata);
-      navigate(Paths.HOME);
-      console.log("User signed in successfully!");
-      console.log("user: ", user);
-    } catch (err) {
-      console.error("Error logging in: ", err);
-      console.log("Error code: ", err.code);
-    }
-  };
+			setUser(userdata);
+			navigate(Paths.HOME);
+			console.log("User signed in successfully!");
+			console.log("user: ", user);
+		} catch (err) {
+			console.error("Error logging in: ", err);
+			console.log("Error code: ", err.code);
+		}
+	};
 
-  // const logout = async () => {
-  //   try {
-  //     await signOut(auth);
-  //     console.log("User logged out successfully!");
-  //   } catch (err) {
-  //     console.error("Error logging out: ", err);
-  //   }
-  // };
+	// const logout = async () => {
+	//   try {
+	//     await signOut(auth);
+	//     console.log("User logged out successfully!");
+	//   } catch (err) {
+	//     console.error("Error logging out: ", err);
+	//   }
+	// };
 
-  return (
-    <>
-      <div className="Image">
-        <img src={Logo} className="App-logo" alt="logo" />
-      </div>
-      <div className="Inputfield">
-        <div>
-          <Label htmlFor="textfield-normal" label="Username" />
-          <Input
-            id="textfield-normal"
-            placeholder="e-mail ID"
-            autoComplete="off"
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div>
-          <Label htmlFor="textfield-password" label="Password" />
-          <Input
-            type="password"
-            placeholder="Password"
-            id="textfield-password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
+	return (
+		<>
+			<div className="Image">
+				<img src={Logo} className="App-logo" alt="logo" />
+			</div>
+			<div className="Inputfield">
+				<div>
+					<Label htmlFor="textfield-normal" label="Username" />
+					<Input
+						id="textfield-normal"
+						placeholder="e-mail ID"
+						autoComplete="off"
+						onChange={(e) => setEmail(e.target.value)}
+					/>
+				</div>
+				<div>
+					<Label htmlFor="textfield-password" label="Password" />
+					<Input
+						type="password"
+						placeholder="Password"
+						id="textfield-password"
+						onChange={(e) => setPassword(e.target.value)}
+					/>
+				</div>
 
-        {/* <input
+				{/* <input
 				placeholder="Enter your Email ID"
 				onChange={(e) => setEmail(e.target.value)}
 			/>
@@ -80,11 +84,11 @@ export default function LoginPage() {
 				type="password"
 				onChange={(e) => setPassword(e.target.value)}
 			/> */}
-        <Button id="SignInButton" onClick={signIn}>
-          Sign In
-        </Button>
-        {/* <Button onClick={logout}>Logout</Button> */}
-      </div>
-    </>
-  );
+				<Button id="SignInButton" onClick={signIn}>
+					Sign In
+				</Button>
+				{/* <Button onClick={logout}>Logout</Button> */}
+			</div>
+		</>
+	);
 }
