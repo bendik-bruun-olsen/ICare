cf02ce3692d13aebef12ad84239d30697af78ab7
Bendik Haakestad Bruun-Olsen <bendik.bruun-olsen@bouvet.no>
2024-06-21 14:24:09 +0200

    Updated Navbar to work with props for left and center content (right is persistent throughout)

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,27 +1,29 @@
 import styles from "./Navbar.module.css";
 import { Icon } from "@equinor/eds-core-react";
 import { account_circle } from "@equinor/eds-icons";
-import Logo from "../Logo/Logo";
 import { useAuth } from "../../hooks/useAuth/useAuth";
 import { doSignOut } from "../../firebase/auth";
+import { ReactNode } from "react";
 
-export default function Navbar({ pageTitle }: { pageTitle: string }) {
+interface NavbarProps {
+	leftContent: ReactNode;
+	centerContent: string;
+}
+
+export default function Navbar({ leftContent, centerContent }: NavbarProps) {
 	const { currentUser } = useAuth();
 
 	return (
 		<nav className={styles.navbar}>
-			<Logo />
-			<div className={styles.pageTitleContainer}>
-				<h1>{pageTitle ? pageTitle : "Missing title"}</h1>
-			</div>
-			<div className={styles.userContainer}>
+			<div className={styles.leftContent}>{leftContent}</div>
+			<div className={styles.centerContent}>{centerContent}</div>
+			<div className={styles.rightContent}>
 				<Icon
 					className={styles.userIcon}
 					data={account_circle}
 					size={40}
 					onClick={doSignOut}
 				/>
-				{/* Split is temporary, until name can be fetched from the database */}
 				<p>{currentUser?.email?.split("@")[0]}</p>
 			</div>
 		</nav>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,15 +1,14 @@
-
 import DatePickerComponent from "../components/DatePicker/DatePickerComponent";
-
+import Navbar from "../components/Navbar/Navbar";
 
 const Home = () => {
-  return (
-    <div>
-      <h1>Homepage</h1>
-      <DatePickerComponent />
-     
-    </div>
-  );
+	return (
+		<>
+			<Navbar leftContent={<h2>LogoTest</h2>} centerContent="Home" />
+			<h1>Homepage</h1>
+			<DatePickerComponent />
+		</>
+	);
 };
 
 export default Home;
