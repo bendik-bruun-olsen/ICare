30344cbf894167774e65d2e5d6228617117f4ce0
Jonas Alexander Støbakk <jonas.stobakk@bouvet.no>
2024-06-24 13:22:36 +0200

    Added a checkbox

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,12 @@ import React from "react";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes";
 import { StateContextProvider } from "./context/StateContext";
-import TaskContainer from "./components/taskcontainer/TaskContainer";
-
-<StateContextProvider>
-	<RouterProvider router={router} />;
-</StateContextProvider>;
 
 const App: React.FC = () => {
 	return (
-		// <StateContextProvider>
-		// 	<RouterProvider router={router} />;
-		// </StateContextProvider>
-		<TaskContainer
-			toDoTitle="hello"
-			toDoComment="hi"
-			toDoDescription="bte"
-			taskStatus="default"
-		/>
+		<StateContextProvider>
+			<RouterProvider router={router} />;
+		</StateContextProvider>
 	);
 };
 
