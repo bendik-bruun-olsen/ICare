149ba36b858ac41573580c084acd634468f1a205
Jonas Alexander Støbakk <jonas.stobakk@bouvet.no>
2024-07-03 14:43:48 +0200

    Changed some names

--- src/components/HomeButton/HomeButton.tsx
+++ src/components/HomeButton/HomeButton.tsx
@@ -2,20 +2,15 @@ import { Icon } from "@equinor/eds-core-react";
 import { home } from "@equinor/eds-icons";
 import { useNavigate } from "react-router-dom";
 import { Paths } from "../../paths";
-import styles from "./HomeButton.module.css";
+import styles from "./Homebutton.module.css";
 
 export default function HomeButton() {
-    const navigate = useNavigate();
-    const handleClick = () => {
-        navigate(Paths.HOME);
-    };
+	const navigate = useNavigate();
+	const handleClick = () => {
+		navigate(Paths.HOME);
+	};
 
-    return (
-        <Icon
-            className={styles.icon}
-            data={home}
-            size={40}
-            onClick={handleClick}
-        />
-    );
+	return (
+		<Icon className={styles.icon} data={home} size={40} onClick={handleClick} />
+	);
 }
