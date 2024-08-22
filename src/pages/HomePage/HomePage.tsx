3ed045f448518d507d6267b9e01bc4ffa5d43ce5
Jonas Alexander Støbakk <jonas.stobakk@bouvet.no>
2024-08-22 09:08:50 +0200

    Changed some index.css stuff

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -3,25 +3,26 @@ import Logo from "../../components/Logo/Logo";
 import PatientDetails from "../../components/PatientDetails/PatientDetails";
 import AppointmentsQuickView from "../../components/AppointmentsQuickView/AppointmentsQuickView";
 import RemainingTodos from "../../components/RemainingTodos/RemainingTodos";
+import style from "./HomePage.module.css";
 
 const HomePage = () => {
-  return (
-    <>
-      <Navbar leftContent={<Logo />} centerContent="Home" />
-      <div className="pageWrapper">
-        <div className="pageContent">
-          <PatientDetails patientName="Gjertrud" age="99" />
-          <AppointmentsQuickView
-            firstAppointment="Doctor's appointment"
-            firstAppointmentTime="09:30"
-            secondAppointment="Physical therapy"
-            secondAppointmentTime="12:00"
-          />
-          <RemainingTodos />
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<Navbar leftContent={<Logo />} centerContent="Home" />
+			<div className={style.pageContainer}>
+				<div className={style.pageContent}>
+					<PatientDetails patientName="Gjertrud" age="99" />
+					<AppointmentsQuickView
+						firstAppointment="Doctor's appointment"
+						firstAppointmentTime="09:30"
+						secondAppointment="Physical therapy"
+						secondAppointmentTime="12:00"
+					/>
+					<RemainingTodos />
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default HomePage;
