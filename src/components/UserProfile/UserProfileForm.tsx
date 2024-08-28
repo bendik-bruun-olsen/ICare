a55100b4592cc5a66a1e65a498bd311df013342b
Sailakshmi Navaneetam <sailakshmi.navaneetam@bouvet.no>
2024-08-28 15:24:08 +0200

    updated unserprofile

--- src/components/UserProfile/UserProfileForm.tsx
+++ src/components/UserProfile/UserProfileForm.tsx
@@ -1,17 +1,19 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef, useCallback } from "react";
 import { Input, Label, NativeSelect, Icon } from "@equinor/eds-core-react";
 import { edit } from "@equinor/eds-icons";
 import { db } from "../../firebase/firebase";
 import { doc, getDoc, updateDoc } from "firebase/firestore";
 import { useAuth } from "../../hooks/useAuth/useAuth";
 import { UserData } from "../../types";
-import styles from "./UserProfileForm.module.css"; // Importing the CSS module
+import styles from "./UserProfileForm.module.css";
 
 const UserProfileForm: React.FC = () => {
   const [userData, setUserData] = useState<UserData | null>(null);
   const [isEditing, setIsEditing] = useState(false);
+  const [isChanged, setIsChanged] = useState(false); // Track if data has changed
   const { currentUser } = useAuth();
   const fullInfoContainerRef = useRef<HTMLDivElement>(null);
+  const timerRef = useRef<NodeJS.Timeout | null>(null);
 
   useEffect(() => {
     const fetchUserData = async () => {
@@ -30,42 +32,83 @@ const UserProfileForm: React.FC = () => {
     fetchUserData();
   }, [currentUser]);
 
+  const saveDataToFirebase = useCallback(async () => {
+    if (currentUser?.email && userData && isChanged) {
+      try {
+        const userDocRef = doc(db, "users", currentUser.email);
+        await updateDoc(userDocRef, { ...userData });
+        console.log("Document successfully updated!");
+        setIsChanged(false); // Reset the changed state after saving
+      } catch (error) {
+        console.error("Error updating document:", error);
+      }
+    }
+  }, [currentUser, userData, isChanged]);
+
+  useEffect(() => {
+    if (timerRef.current) {
+      clearTimeout(timerRef.current);
+    }
+
+    timerRef.current = setTimeout(() => {
+      if (isChanged) {
+        saveDataToFirebase();
+      }
+    }, 2000);
+
+    return () => {
+      if (timerRef.current) {
+        clearTimeout(timerRef.current);
+      }
+    };
+  }, [userData, saveDataToFirebase, isChanged]);
+
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
       if (
         fullInfoContainerRef.current &&
         !fullInfoContainerRef.current.contains(event.target as Node)
       ) {
+        if (isChanged) {
+          saveDataToFirebase();
+        }
         setIsEditing(false);
       }
     };
 
+    const handleBeforeUnload = () => {
+      if (isChanged) {
+        saveDataToFirebase();
+      }
+    };
+
     document.addEventListener("mousedown", handleClickOutside);
+    window.addEventListener("beforeunload", handleBeforeUnload);
 
     return () => {
       document.removeEventListener("mousedown", handleClickOutside);
+      window.removeEventListener("beforeunload", handleBeforeUnload);
     };
-  }, []);
+  }, [userData, saveDataToFirebase, isChanged]);
 
   const handleEditClick = () => {
     setIsEditing(true);
   };
 
-  const handleChange = async (
+  const handleChange = (
     e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
   ) => {
     const { id, value } = e.target;
-    const updatedData = { ...userData, [id]: value } as UserData;
-    setUserData(updatedData);
 
-    if (currentUser?.email) {
-      try {
-        const userDocRef = doc(db, "users", currentUser.email);
-        await updateDoc(userDocRef, { [id]: value });
-      } catch (error) {
-        console.error("Error updating document:", error);
-      }
-    }
+    setUserData(
+      (prevData) =>
+        ({
+          ...prevData,
+          [id]: value,
+        } as UserData)
+    );
+
+    setIsChanged(true);
   };
 
   return (
@@ -79,13 +122,14 @@ const UserProfileForm: React.FC = () => {
         />
       </div>
       <div className={styles.inputGroup}>
-        <Label htmlFor="name" label="Name*" />
+        <Label htmlFor="name" label="Name" />
         <Input
           id="name"
           type="text"
           value={userData?.name || ""}
           onChange={handleChange}
           readOnly={!isEditing}
+          className={isEditing ? styles.editable : ""}
         />
       </div>
       <div className={styles.inputGroup}>
@@ -96,6 +140,7 @@ const UserProfileForm: React.FC = () => {
           value={userData?.age || ""}
           onChange={handleChange}
           readOnly={!isEditing}
+          className={isEditing ? styles.editable : ""}
         />
       </div>
       <div className={styles.inputGroup}>
@@ -106,6 +151,7 @@ const UserProfileForm: React.FC = () => {
           value={userData?.gender || ""}
           onChange={handleChange}
           disabled={!isEditing}
+          className={isEditing ? styles.editable : ""}
         >
           <option value="">Select Gender</option>
           <option value="Male">Male</option>
@@ -121,6 +167,7 @@ const UserProfileForm: React.FC = () => {
           value={userData?.phone || ""}
           onChange={handleChange}
           readOnly={!isEditing}
+          className={isEditing ? styles.editable : ""}
         />
       </div>
       <div className={styles.inputGroup}>
