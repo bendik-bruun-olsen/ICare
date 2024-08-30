e1d285207a9a94d35b674802ade7501bd28bc6bc
Kokila Chandramohan <kokila.chandramohan@bouvet.no>
2024-08-30 12:55:35 +0200

    Fixed the red star

--- src/pages/RecoverPassword/RecoverPasswordPage.tsx
+++ src/pages/RecoverPassword/RecoverPasswordPage.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Input, Button, InputWrapper } from "@equinor/eds-core-react";
 import BannerImage from "../../assets/images/Logo.png";
 import Logo from "../../components/Logo/Logo";
-import "./RecoverPasswordPage.modules.css";
+import styles from "./RecoverPasswordPage.module.css";
 import styled from "styled-components";
 import { sendResetEmail } from "../../utils";
 import { checkUserExists } from "../../utils";
@@ -10,82 +10,82 @@ import { useNotification } from "../../hooks/useNotification";
 import Loading from "../../components/Loading/Loading";
 
 const CustomButton = styled(Button)`
-	margin-top: 1rem;
+  margin-top: 1rem;
 
-	width: 100%;
+  width: 100%;
 
-	background-color: var(--blue);
-	color: var(--white);
+  background-color: var(--blue);
+  color: var(--white);
 `;
 const CustomInputWrapper = styled(InputWrapper)`
-	margin-top: 1rem;
-	width: 100%;
-	color: var(--black);
+  margin-top: 1rem;
+  width: 100%;
+  color: var(--black);
 `;
 
 export default function RecoverPasswordPage() {
-	const [email, setEmail] = useState<string>("");
-	const [message] = useState<string>("");
-	const { addNotification } = useNotification();
-	const [isLoading, setIsLoading] = useState(false);
+  const [email, setEmail] = useState<string>("");
+  const [message] = useState<string>("");
+  const { addNotification } = useNotification();
+  const [isLoading, setIsLoading] = useState(false);
 
-	const handleForgotPassword = async (e: React.FormEvent) => {
-		e.preventDefault();
+  const handleForgotPassword = async (e: React.FormEvent) => {
+    e.preventDefault();
 
-		const exists = await checkUserExists(email);
-		try {
-			setIsLoading(true);
-			if (exists) {
-				await sendResetEmail(email);
-				addNotification("Password reset email sent!", "success");
-			} else {
-				addNotification("User does not exist", "info");
-			}
-		} catch (error) {
-			addNotification("Error sending Email.", "error");
-		} finally {
-			setIsLoading(false);
-		}
-	};
+    const exists = await checkUserExists(email);
+    try {
+      setIsLoading(true);
+      if (exists) {
+        await sendResetEmail(email);
+        addNotification("Password reset email sent!", "success");
+      } else {
+        addNotification("User does not exist", "info");
+      }
+    } catch (error) {
+      addNotification("Error sending Email.", "error");
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
-	const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-		setEmail(e.target.value);
-	};
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
 
-	if (isLoading) return <Loading />;
+  if (isLoading) return <Loading />;
 
-	return (
-		<div className="pageWrapper">
-			<div className="heading">
-				<Logo size={"70px"} color={"var(--blue)"} />
-			</div>
+  return (
+    <div className="pageWrapper">
+      <div className="heading">
+        <Logo size={"70px"} color={"var(--blue)"} />
+      </div>
 
-			<img src={BannerImage} alt="logo-image" className="bannerImage" />
+      <img src={BannerImage} alt="logo-image" className={styles.bannerImage} />
 
-			<form onSubmit={handleForgotPassword} className="InputContainer">
-				<CustomInputWrapper
-					className="input1"
-					labelProps={{
-						label: "Email",
-						htmlFor: "textfield-normal",
-						style: { display: "block" },
-					}}
-				>
-					<Input
-						type="email"
-						id="email"
-						value={email}
-						onChange={handleEmailChange}
-						required
-					/>
-				</CustomInputWrapper>
+      <form onSubmit={handleForgotPassword} className={styles.InputContainer}>
+        <CustomInputWrapper
+          className="input1"
+          labelProps={{
+            label: "Email",
+            htmlFor: "textfield-normal",
+            style: { display: "block" },
+          }}
+        >
+          <Input
+            type="email"
+            id="email"
+            value={email}
+            onChange={handleEmailChange}
+            required
+          />
+        </CustomInputWrapper>
 
-				<CustomButton id="sendEmail" type="submit">
-					Send Email
-				</CustomButton>
-			</form>
+        <CustomButton id="sendEmail" type="submit">
+          Send Email
+        </CustomButton>
+      </form>
 
-			{message && <p>{message}</p>}
-		</div>
-	);
+      {message && <p>{message}</p>}
+    </div>
+  );
 }
