d894de7ff064d629bed8f7776ee7ba0ca0a5dbea
Jonas Alexander Støbakk <jonas.stobakk@bouvet.no>
2024-09-17 14:39:45 +0200

    Fixed created by

--- src/firebase/patientServices/addPatient.ts
+++ src/firebase/patientServices/addPatient.ts
@@ -1,15 +1,32 @@
 import { collection, doc, setDoc } from "firebase/firestore";
 import { Caretaker, NewPatient } from "../../types";
 import { db } from "../firebase";
+import { getAuth } from "firebase/auth";
 
 export const addPatient = async (
 	formData: NewPatient,
 	caretakers: Caretaker[],
 	id?: string
 ): Promise<string> => {
+	const auth = getAuth();
+	const currentUser = auth.currentUser;
+
+	if (!currentUser) {
+		throw new Error("No user is currently logged in.");
+	}
+
 	const patientRef = id
 		? doc(db, "patientdetails", id)
 		: doc(collection(db, "patientdetails"));
-	await setDoc(patientRef, { ...formData, caretakers });
+
+	const patientData = {
+		...formData,
+		caretakers,
+		createdBy: {
+			userEmail: currentUser.email,
+		},
+	};
+
+	await setDoc(patientRef, patientData);
 	return patientRef.id;
 };
