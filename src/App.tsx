311d5619bbf880e108ba0cbfe374d232ed0afc64
Anne Skjæveland <anne.skjeveland@bouvet.no>
2024-10-23 11:52:17 +0200

    add back snackbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import router from "./routes";
 import { RouterProvider } from "react-router-dom";
 import { AuthProvider } from "./hooks/useAuth/useAuth";
 import { NotificationProvider } from "./context/NotificationContext";
+import Snackbar from "./components/SnackBar/Snackbar";
 
 export default function App(): JSX.Element {
   return (
     <AuthProvider>
       <NotificationProvider>
         <RouterProvider router={router} />
+        <Snackbar />
       </NotificationProvider>
     </AuthProvider>
   );
