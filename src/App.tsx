101372e61aeeeecda075c0dd476010e77ac554bb
Anne Skjæveland <anne.skjeveland@bouvet.no>
2024-10-23 11:50:26 +0200

    temp remove

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,13 @@ import router from "./routes";
 import { RouterProvider } from "react-router-dom";
 import { AuthProvider } from "./hooks/useAuth/useAuth";
 import { NotificationProvider } from "./context/NotificationContext";
-import Snackbar from "./components/SnackBar/Snackbar";
 
 export default function App(): JSX.Element {
-	return (
-		<AuthProvider>
-			<NotificationProvider>
-				<RouterProvider router={router} />
-				<Snackbar />
-			</NotificationProvider>
-		</AuthProvider>
-	);
+  return (
+    <AuthProvider>
+      <NotificationProvider>
+        <RouterProvider router={router} />
+      </NotificationProvider>
+    </AuthProvider>
+  );
 }
